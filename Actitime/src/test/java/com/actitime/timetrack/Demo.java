5f75fd118414fdbf29904aed39f9d82a895c9f8f
OM NAMAH SHIVAYA <OM NAMAH SHIVAYA@DESKTOP-ILACP4G>
2020-08-09 17:17:53 +0530

    My Project Commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,20 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest()
+	{
+		Reporter.log("successfully running",true);
+		
+		
+	}
+	
+	
+	
+	
+	
+}
