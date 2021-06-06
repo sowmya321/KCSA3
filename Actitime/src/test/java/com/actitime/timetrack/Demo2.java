a524b46932e513a70ef75a28c7ce6fcc49d1716e
OM NAMAH SHIVAYA <OM NAMAH SHIVAYA@DESKTOP-ILACP4G>
2021-06-06 22:55:04 +0530

    demo2 commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -0,0 +1,17 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void runTest2()
+	{
+		Reporter.log("successfully running demo2",true);
+		Reporter.log("can you see me in jenkins?",true);
+
+		//hi this is sowmya...
+	
+	
+}
+}
